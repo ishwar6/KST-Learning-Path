3476c602dfc1c2b186dcdf77282872786a94ecc8
Mimichim <iisudrj11@gmail.com>
2018-08-18 01:42:06 +0530

    Creater Model for State and created new app content

--- learningroute/states/models.py
+++ learningroute/states/models.py
@@ -1,6 +1,20 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+
+
+class State(models.Model):
+    title = models.CharField(max_length = 120, blank = False, null = False)
+    topic = models.CharField(max_length = 222)
+    rate = models.IntegerField()
+    time = models.IntegerField()
+    tag  = models.CharField( max_length = 15, blank = True, null = True  )
+
+
+    def __str__(self):
+        return self.title + ' - for the topic - ' + self.topic
+
+
+
+
+
+
