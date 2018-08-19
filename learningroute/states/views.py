b167ee2fa6d51aa5718658b0427bda105da8f070
Ishwarjangid <iisudrj26@gmail.com>
2018-08-19 20:35:40 +0530

    The Actual Knowledge Space working activated....

--- learningroute/states/views.py
+++ learningroute/states/views.py
@@ -21,6 +21,10 @@ def nodes(request):
     r.sets_options('quote', False)
     edges =  Edge.objects.all()
     n = Node.objects.all()
+    print(n)
+
+
+
     i = 0
     set = {}
     # making dictionary of individual state_nodes in set variable
@@ -28,35 +32,33 @@ def nodes(request):
         set[i] = ','.join(str(i.title) for i in node.state_node.all())
         i = i+1
     print(set)
-    r_set = {}
-    #converting the dictionary in R SET's
+    # r_set = {}
+    # #converting the dictionary in R SET's
+    # for key, value in set.items():
+    #     value = value.replace("{","").replace("}", "")
+    #     r_set[key] = r.set(value)
+
+
+    # converting the python SET's dictionary of sets :- (to) set of set
+    a = r.set('')
     for key, value in set.items():
-        value = value.replace("{","").replace("}", "")
-        r_set[key] = r.set(value)
 
-
-    # converting the R SET's dictionary of sets :- (to) set of custom Strings
-    a = []
-    for key, value in r_set.items():
-
-        a.append(value)
-    print(a)
-
+        b = r.set(value)
+        a = r.set_union(a, b)
     # making the knowledge space from above set of strings
-    set_ = r.set(a)
 
+    ks = kst.kstructure(a)
 
-    ks = kst.kstructure(set_)
-
-    print(ks)
-    print('asdf')
-    print(kst.kspace(ks))
+    # print(ks)
+    # print('asdf')
+    ksp = kst.kspace(ks)
+    print(ksp)
     #print(kst.kdomain(ks))
     print(kst.kstructure_is_kspace(ks))
 
     #print(kspace)
-    lp = kst.lpath(ks)
-    print(lp)
+    lp = kst.lpath(ksp)
+
 
 
 
