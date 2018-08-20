ea7a3399397f59afff43d8b39cc258a5c8d14558
Ishwarjangid <iisudrj26@gmail.com>
2018-08-20 15:42:17 +0530

    Userstate Models

--- learningroute/userstates/models.py
+++ learningroute/userstates/models.py
@@ -2,10 +2,16 @@ from django.db import models
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
-# class UserState(models.Model):
-#     user        = models.ForeignKey(User, on_delete=models.CASCADE,default=None)
-#     state       = models.ForeignKey(State, on_delete= models.CASCADE, default = None)
-#     correct_q   = models.IntegerField(default = 0)
-#
-#     time_taken  = models.IntegerField(blank = True, null = True)
-#     timedate    = models.DateTimeField(auto_now_add = True)
+class UserState(models.Model):
+    user        = models.ForeignKey(User, on_delete=models.CASCADE,default=None)
+    state       = models.ForeignKey(State, on_delete= models.CASCADE, default = None)
+    correct_q   = models.IntegerField(default = 0)
+    stage       = models.IntegerField(default = 0)
+    time_taken  = models.IntegerField(blank = True, null = True)
+    timedate    = models.DateTimeField(auto_now_add = True)
+
+
+    def __str__(self):
+        return str(self.user) + ' - ' + str(self.stage)
+
+    
