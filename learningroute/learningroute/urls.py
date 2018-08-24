ebacd08c9f45b58910bd0c610b69b10b308ea149
Ishwarjangid <iisudrj26@gmail.com>
2018-08-24 22:38:46 +0530

    Urls file

--- learningroute/learningroute/urls.py
+++ learningroute/learningroute/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import  static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    
     url(r'^states/', include('states.urls')),
     url(r'^auth/', include('authentication.urls')),
     url(r'^userstates/', include('userstates.urls')),
