29d7980cd2737c6f5946535dd9d3385df0581efd
Ishwarjangid <iisudrj26@gmail.com>
2018-09-14 17:37:54 +0530

    Settings

--- learningroute/learningroute/settings.py
+++ learningroute/learningroute/settings.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     ####### External  Packages###########
-    'rpy2',
+
     ########### Self Added Apps #######
     'states',
     'content',
